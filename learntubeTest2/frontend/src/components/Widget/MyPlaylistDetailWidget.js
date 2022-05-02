9378494a0e070d259723fb412c9040998f59ad80
JihuYang <zihooy139@gmail.com>
2022-05-02 22:24:54 +0900

    learntube studio: myplaylist detail

--- learntubeTest2/frontend/src/components/Widget/MyPlaylistDetailWidget.js
+++ learntubeTest2/frontend/src/components/Widget/MyPlaylistDetailWidget.js
@@ -8,17 +8,10 @@ const MyPlaylistDetailWidget = (props) => {
     return (
         <div className="playlist mb-50">
             <h3 className="widget-title">{playlistTitle}</h3>
-            <Link to="/"
-            ><Playlist_listview
-                playlistTitle='DFS 알고리즘'
-                playlistTime='30'
-            />
-            </Link>
-            <Playlist_listview
-                playlistTitle='BFS 알고리즘'
-                playlistTime='30'
-            />
-
+            {/* <Playlist_listview
+                playlistTitle={playlistTitle}
+                playlistTime='50'
+            /> */}
         </div>
     )
 }
